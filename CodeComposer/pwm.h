cdc853c9c7190ace33f8a505f3d89e11d9523996
Nanu Dahal <55097498+pada8716@users.noreply.github.com>
2019-12-11 11:00:33 -0700

    updating pwm.h comments more

--- CodeComposer/pwm.h
+++ CodeComposer/pwm.h
@@ -14,19 +14,16 @@ void config_pwm_timer2(void);
 
  
 /* Calculates number of ticks required for duty cycle and frequency of Timer_A0*/
-void start_pwm(uint8_t duty_cycle, uint8_t pwm);
+void start_pwm_p2_4(uint8_t pwm);
 
 /* Calculates number of ticks required for duty cycle and frequency of Timer_A2*/
-void start_pwm2(uint8_t duty_cycle, uint8_t pwm);
+void start_pwm_p5_6(uint8_t pwm);
 
 
 /* Config P2.4 to output TA0.1 waveform */
- void config_pwm_gpio(void);
+ void config_pwm_gpio_p2_4(void);
 
 /* Config P5.6 to output TA2.1 waveform */
- void config_pwm_gpio2(void);
- 
-/* Function to delay 4s */
- void delay(void);
+ void config_pwm_gpio_p5_6(void);
 
 #endif
