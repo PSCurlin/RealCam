67588ac8da6cedcaa591406fec4ec58fd7e619c5
Kaitlyn Doan <kado9658@colorado.edu>
2019-12-11 12:41:01 -0700

    pwm comments

--- CodeComposer/pwm.h
+++ CodeComposer/pwm.h
@@ -5,28 +5,15 @@
 
 
 
-/* Configure TIMER_A0 to produce PWM waveform
+/* Configure TIMER_A0 to produce PWM waveform*/
 
-  * TODO: reset R (timer counter) register
-
-  * - TODO: select SMCLK (3MHz) in the CTL register
-
-  * * - TODO: select reset/set output mode for T0.1 timer using CCTL[1]  */
 
 void config_pwm_timer(void);
 void config_pwm_timer2(void);
 
-/*  - TODO: Start PWM signal on Pin XX at duty_cycle 100kHz,
 
- * *    Note: the DRV2605L PWM input frequency is XXXX
 
- * *  - TODO: calculate and set the amount of ticks needed in CCR
-
- * *  - TODO: enable/start timer (UP mode)
-
- * *  - TODO: Counting and then reset
-
- * * @param uint8_t duty_cycle: 0-100, percentage of time ON */
+/* @param uint8_t duty_cycle: 0-100, percentage of time ON */
 
 void start_pwm(uint8_t duty_cycle, uint8_t pwm);
 
